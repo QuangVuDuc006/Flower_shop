57ff150b78f308e05c208aa20fe8222123dbefb1
QuangVuDuc006 <quangvd2410831@usth.edu.vn>
2025-11-21 00:45:57 +0700

    Revert config to env variable

--- config.py
+++ config.py
@@ -5,10 +5,10 @@ class Config:
     basedir = os.path.abspath(os.path.dirname(__file__))
 
     # Lấy link DB từ biến môi trường (trên Render), nếu ko có thì dùng SQLite (ở nhà)
+
     uri = os.environ.get('DATABASE_URL')
     if uri and uri.startswith("postgres://"):
-        uri = uri.replace("postgres://", "postgresql://", 1) # Fix lỗi SQLAlchemy cũ
-
+        uri = uri.replace("postgres://", "postgresql://", 1)
     SQLALCHEMY_DATABASE_URI = uri or 'sqlite:///' + os.path.join(basedir, 'flower_shop.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = os.path.join(basedir, 'static/uploads')
